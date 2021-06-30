a267d5795269c9eea3651f41022659749d09de81
Ntwali Raymond <rntwali8@gmail.com>
2021-06-30 23:52:28 +0300

    completed ui for signin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/error/NotFound.js"
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={Signin}></Route>
         <Route path='/Dashboard' component={Dashboard}></Route>
+        <Route path='/404' component={NotFound}></Route>
+        <Redirect to='/404' />
       </Switch>
       </div>
     </Router>
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,8 +1,69 @@
 
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Col, Row, Form, Button, Container, InputGroup } from "react-bootstrap";
+
+import BgImage from "../assets/images/signin.svg";
+
 export default () => {
-	return (
-		<h1>
-			This is the signin page!
-		</h1>
-	);
-}
+  return (
+    <main>
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container>
+          <Row className="justify-content-center form-bg-image"
+            style={{ backgroundImage: `url(${BgImage})`, backgroundRepeat: 'no-repeat' }}
+          >
+            <Col xs={12} md={5}  className="d-flex align-items-center justify-content-center">
+              <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                <div className="text-center text-md-center mb-4 mt-md-0">
+                  <h3 className="mb-0">Sign in to our platform</h3>
+                </div>
+                <Form className="mt-4">
+                  {/* Email section */}
+                  <Form.Group id="email" className="mb-4">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </InputGroup.Text>
+                      <Form.Control autoFocus required type="email" placeholder="example@company.com" />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    {/* Password section */}
+                    <Form.Group id="password" className="mb-4">
+                      <Form.Label>Your Password</Form.Label>
+                      <InputGroup>
+                        <InputGroup.Text>
+                          <FontAwesomeIcon icon={faUnlockAlt} />
+                        </InputGroup.Text>
+                        <Form.Control required type="password" placeholder="Password" />
+                      </InputGroup>
+                    </Form.Group>
+                  </Form.Group>
+                  <Button variant="dark" type="submit" className="w-100">
+                    Sign in
+                  </Button>
+                </Form>
+                {/* Check-out out sites */}
+                <div className="mt-3 mb-4 text-center">
+                  <span className="fw-normal">Find me at</span>
+                </div>
+                <div className="d-flex justify-content-center my-4">
+                  <Button variant="outline-secondary" className="btn-icon-only btn-pill text-facebook me-2">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                  </Button>
+                  <Button variant="outline-secondary" className="btn-icon-only btn-pill text-twitter me-2">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
+};
