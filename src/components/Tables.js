b438e618f32a8ae579b02818628a105a988f8d25
David OH <holifeilddavid@gmail.com>
2021-07-05 12:49:12 +0300

    Dashboard Elements Added

---
+++ src/components/Tables.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { Card, Table, Button } from "@themesberg/react-bootstrap";
+
+import { pageVisits } from "../data/tables";
+
+export const PageVisitsTable = () => {
+  const TableRow = (props) => {
+    const { pageName, views, returnValue, bounceRate } = props;
+    const bounceIcon = bounceRate < 0 ? faArrowDown : faArrowUp;
+    const bounceTxtColor = bounceRate < 0 ? "text-danger" : "text-success";
+
+    return (
+      <tr>
+        <th scope="row">{pageName}</th>
+        <td>{views}</td>
+        <td>${returnValue}</td>
+        <td>
+          <FontAwesomeIcon
+            icon={bounceIcon}
+            className={`${bounceTxtColor} me-3`}
+          />
+          {Math.abs(bounceRate)}%
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <Card border="light" className="shadow-sm">
+      <Card.Header>
+        <div className="d-flex align-items-center">
+          <div>
+            <h5>Page visits</h5>
+          </div>
+          <div className="d-flex ml-auto">
+            <Button variant="info" size="sm" className="mr-0">
+              See all
+            </Button>
+          </div>
+        </div>
+      </Card.Header>
+      <Table responsive className="align-items-center table-flush">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">Page name</th>
+            <th scope="col">Page Views</th>
+            <th scope="col">Page Value</th>
+            <th scope="col">Bounce rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pageVisits.map((pv) => (
+            <TableRow key={`page-visit-${pv.id}`} {...pv} />
+          ))}
+        </tbody>
+      </Table>
+    </Card>
+  );
+};
--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import { Card, Button } from "@themesberg/react-bootstrap";
-import { SalesValueChart } from "./Charts";
+import {
+  faAngleDown,
+  faAngleUp,
+  faChartBar,
+  faChartArea,
+} from "@fortawesome/free-solid-svg-icons";
+import { Row, Col, Card, Button } from "@themesberg/react-bootstrap";
+import { SalesValueChart, CircleChart } from "./Charts";
 
 export const SalesValueWidget = (props) => {
   const { title, value, percentage } = props;
@@ -28,16 +33,88 @@ export const SalesValueWidget = (props) => {
           </small>
         </div>
         <div className="d-flex ml-auto">
-          <Button variant="secondary" size="sm" className="me-2 mr-2">
+          <Button variant="info" size="sm" className="me-2 mr-2">
             Month
           </Button>
-          <Button variant="primary" size="sm" className="me-3 mr-2">
+          <Button variant="secondary" size="sm" className="me-3 mr-2">
             Week
           </Button>
         </div>
       </Card.Header>
       <Card.Body className="p-2">
         <SalesValueChart />
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const CircleChartWidget = (props) => {
+  const { title, data = [] } = props;
+  const series = data.map((d) => d.value);
+
+  return (
+    <Card border="light" className="shadow-sm mb-3">
+      <Card.Body>
+        <Row className="d-block d-xl-flex align-items-center">
+          <Col
+            xs={12}
+            xl={5}
+            className="text-xl-center d-flex align-items-center justify-content-xl-center mb-3 mb-xl-0"
+          >
+            <CircleChart series={series} />
+          </Col>
+          <Col xs={12} xl={7} className="px-xl-0">
+            <h5 className="mb-3">{title}</h5>
+
+            {data.map((d) => (
+              <h6
+                key={`circle-element-${d.id}`}
+                className="fw-normal text-gray"
+              >
+                <FontAwesomeIcon
+                  icon={d.icon}
+                  className={`icon icon-xs text-${d.color} w-20 me-1`}
+                />
+                {` ${d.label} `}
+                {`${d.value}%`}
+              </h6>
+            ))}
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const AcquisitionWidget = () => {
+  return (
+    <Card border="light" className="shadow-sm mt-3">
+      <Card.Body>
+        <h5>Acquisition</h5>
+        <p>
+          Tells you where your visitors originated from, such as search engines,
+          social networks or website referrals.
+        </p>
+        <div className="d-block">
+          <div className="d-flex align-items-center pt-3 me-5">
+            <div className="icon icon-shape icon-sm icon-shape-danger rounded me-3">
+              <FontAwesomeIcon icon={faChartBar} />
+            </div>
+            <div className="d-block ml-3">
+              <label className="mb-0">Bounce Rate</label>
+              <h4 className="mb-0">33.50%</h4>
+            </div>
+          </div>
+          <div className="d-flex align-items-center pt-3">
+            <div className="icon icon-shape icon-sm icon-shape-quaternary rounded me-3">
+              <FontAwesomeIcon icon={faChartArea} />
+            </div>
+            <div className="d-block ml-3">
+              <label className="mb-0">Sessions</label>
+              <h4 className="mb-0">9,567</h4>
+            </div>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
